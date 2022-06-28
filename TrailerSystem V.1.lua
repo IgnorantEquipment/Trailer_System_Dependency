4e5706fb20128c7e48da371e066c40383d72300d
IgnorantEquipment <104283241+IgnorantEquipment@users.noreply.github.com>
2022-06-27 23:06:29 -0400

    Update TrailerSystem V.1.lua

--- TrailerSystem V.1.lua
+++ TrailerSystem V.1.lua
@@ -138,7 +138,7 @@ end
 while wait() do
 	if not RunService:IsEdit() then
 		print("Running")
-		
+		print(game.Workspace:FindFirstChild("Baseplate"))
 		if Selection:Get()[1] ~= nil then
 		print(Selection:Get()[1])
 			if table.find(Trailers,Selection:Get()[1]) then
